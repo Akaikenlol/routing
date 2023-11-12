34b28eb4d0faa0b6049623de7dd123ccd450d562
Akaikenlol <keithken972@gmail.com>
2023-11-12 23:45:10 +0630

    nested route

---
+++ app/project/list/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Page = () => {
+	return (
+		<main className="flex justify-center items-center p-8 flex-col">
+			<h1>Project List Page</h1>
+		</main>
+	);
+};
+
+export default Page;
